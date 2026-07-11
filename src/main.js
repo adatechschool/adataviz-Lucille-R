487cf7f4112e07b8e9a00601e6101d5d31aac53f
Lucille-R <lucillerichard37@yahoo.fr>
2026-07-11 15:57:39 +0200

    Chargement des données depuis l'API

--- src/main.js
+++ src/main.js
@@ -1,25 +1,25 @@
 // fetch + manipulation du DOM (pas testé)
-// Je récupère ma div cards :
+
+// Je récupère ma section cards-hotels :
 const cardsHotels = document.querySelector(".cards-hotels");
 
-// Je déclare ma fonction async :
-const chargerDonnees = async () => {
+// Chargement des données : Je déclare ma fonction async :
+const donneesChargees = async () => {
 	try {
 		// Je récupère les données de l'API grâce à fetch :
-		const response = await fetch("https://data.tours-metropole.fr/api/explore/v2.1/catalog/datasets/hotels-en-region-centre-val-de-loire/records?limit=10");
+		const response = await fetch("https://data.tours-metropole.fr/api/explore/v2.1/catalog/datasets/hotels-en-region-centre-val-de-loire/records?limit=20");
 
 		// Je transforme les données récupérées en JSON :
 		const donnees = await response.json();
 		console.log(donnees) // => Pour vérifier que l'on récupère les données
-
-		// J'itère sur l'array :
-		donnees.results.forEach((hotel) => {
-			console.log(hotel.nom_offre)
-		})
-	
+		
 	} catch (erreur) {
-		console.error(erreur.message)
+		console.error(`Impossible de récupérer les données : ${erreur.message}`)
 	}
 }
 
-chargerDonnees()
+donneesChargees()
+// J'itère sur l'array :
+/* donnees.results.forEach((hotel) => {
+	console.log(hotel.nom_offre)
+}) */
