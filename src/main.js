c50ea5c308876584eadbede155316f1da8af6451
Lucille-R <lucillerichard37@yahoo.fr>
2026-07-11 18:05:56 +0200

    Affichage dynamique des cards en JS + modif CSS sur les cards

--- src/main.js
+++ src/main.js
@@ -24,16 +24,47 @@ const donneesChargees = async () => {
 
 // -------- Utilisation des données de l'API pour création des cards d'hôtels -------- 
 const afficherHotels = (donneesHotels) => {
+
 	// Je parcours l'objet donneesHotels :
 	donneesHotels.results.forEach((hotel) => {
-		console.log(hotel.nom_offre);
+
+		// 1- Je crée le conteneur <div> pour l'insertion des card par hôtel, et je lui donne sa class "card" :
+		const divCard = document.createElement("div");
+		divCard.classList.add("card");
+		// Le conteneur parent (section cards-hotels) est déjà sélectionné tout en haut, j'y insère mon div :
+		cardsHotels.appendChild(divCard);
+
+		// 2- Je crée le h2 pour y mettre le nom de l'hôtel :
+		const nomHotel = document.createElement("h2");
+		nomHotel.textContent = hotel.nom_offre;
+		// Je l'insère dans le conteneur divCard :
+		divCard.appendChild(nomHotel);
+
+		// 3- Je crée un p pour y mettre l'adresse de l'hôtel :
+		const pAdresse = document.createElement("p");
+		pAdresse.textContent = hotel.adresse_1;
+		// Je l'insère dans le conteneur divCard :
+		divCard.appendChild(pAdresse);
+
+		// 4- Je crée un p pour y mettre la commune de l'hôtel :
+		const pCommune = document.createElement("p");
+		pCommune.textContent = hotel.commune;
+		// Je l'insère dans le conteneur divCard :
+		divCard.appendChild(pCommune);
+
+		// 5- Je crée un <a> pour y mettre le site de l'hôtel :
+		const aSite = document.createElement("a");
+		// Je lui affecte un attribut href car c'est un lien vers le site Internet de l'hôtel :
+		aSite.setAttribute("href", "hotel.site_web");
+		// ainsi que son texte :
+		aSite.textContent = hotel.site_web;
+		// Je l'insère dans le conteneur divCard :
+		divCard.appendChild(aSite);
+
 	})
 }
 
-// J'itère sur l'array :
-/* donnees.results.forEach((hotel) => {
-	console.log(hotel.nom_offre)
-}) */
+// -------- Fonction main qui fait le lien entre donneesChargees et afficherHotel (sinon pbm de scope)  -------- 
  const main = async () => {
 	let donneesHotels = await donneesChargees();
 	afficherHotels(donneesHotels);
